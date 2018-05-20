4577bea740175e6f2a6158ef015d64b6c61e5446
Sergey Kostyan <>
2018-05-20 15:10:28 +0300

    opens your app settings

--- AppSettingsShowing/Sources/AppSettingsShowingInterface.swift
+++ AppSettingsShowing/Sources/AppSettingsShowingInterface.swift
@@ -19,7 +19,10 @@ public protocol AppSettingsShowingInterface: AlertControllerShowingInterface {
 public extension AppSettingsShowingInterface {
 
     func showAppSettings(completion: ((Bool) -> Void)? = nil) {
-        guard let settingsAppURL = URL(string: UIApplicationOpenSettingsURLString), UIApplication.shared.canOpenURL(settingsAppURL) else { return }
+        guard let bundleString = Bundle.main.bundleIdentifier, let settingsAppURL = URL(string: UIApplicationOpenSettingsURLString + bundleString),
+            UIApplication.shared.canOpenURL(settingsAppURL) else {
+            completion?(false)
+            return }
         UIApplication.shared.open(settingsAppURL, options: [:], completionHandler: completion)
     }
     
