da5350521efac4061b45f98e3ff38e0cad7aec3a
Sergey <sjsoad666@yandex.ru>
2018-09-18 12:02:25 +0300

    swift 4.2

--- AppSettingsShowing/Sources/AppSettingsShowingInterface.swift
+++ AppSettingsShowing/Sources/AppSettingsShowingInterface.swift
@@ -22,14 +22,19 @@ public extension AppSettingsShowingInterface {
         guard let myAppSettingsURL = myAppSettingsURL, UIApplication.shared.canOpenURL(myAppSettingsURL) else {
             completion?(false)
             return }
-        UIApplication.shared.open(myAppSettingsURL, options: [:], completionHandler: completion)
+        UIApplication.shared.open(myAppSettingsURL, options: convertToUIApplicationOpenExternalURLOptionsKeyDictionary([:]), completionHandler: completion)
     }
     
     // MARK: - Private -
     
     private var myAppSettingsURL: URL? {
-        let urlString = [UIApplicationOpenSettingsURLString, Bundle.main.bundleIdentifier].compactMap({ $0 }).joined()
+        let urlString = [UIApplication.openSettingsURLString, Bundle.main.bundleIdentifier].compactMap({ $0 }).joined()
         return URL(string: urlString)
     }
     
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertToUIApplicationOpenExternalURLOptionsKeyDictionary(_ input: [String: Any]) -> [UIApplication.OpenExternalURLOptionsKey: Any] {
+	return Dictionary(uniqueKeysWithValues: input.map { key, value in (UIApplication.OpenExternalURLOptionsKey(rawValue: key), value)})
 }
