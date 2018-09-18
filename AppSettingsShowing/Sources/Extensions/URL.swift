9a926a3753c219bbabc9f6c05980eadcfacd1ef4
Sergey <sjsoad666@yandex.ru>
2018-09-18 13:06:33 +0300

    swift 4.2

--- AppSettingsShowing/Sources/Extensions/URL.swift
+++ AppSettingsShowing/Sources/Extensions/URL.swift
@@ -9,16 +9,13 @@ import Foundation
 
 extension URL {
     
-    #if swift(>=4.2)
+    
     static var appSettings: URL? {
+        #if swift(>=4.2)
         let urlString = [UIApplication.openSettingsURLString, Bundle.main.bundleIdentifier].compactMap({ $0 }).joined()
+        #else
+        let urlString = [UIApplicationOpenSettingsURLString, Bundle.main.bundleIdentifier].compactMap({ $0 }).joined()
+        #endif
         return URL(string: urlString)
     }
-    #else
-    static var appSettings: URL? {
-    let urlString = [UIApplicationOpenSettingsURLString, Bundle.main.bundleIdentifier].compactMap({ $0 }).joined()
-    return URL(string: urlString)
-    }
-    #endif
-    
 }
